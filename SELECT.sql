0231ffd8e60a9e639855ea470796f1d91c1ad188
GitWitAnish <anishkarki5710@gmail.com>
2025-05-12 08:46:00 +0545

    SQL SELECT

---
+++ SELECT.sql
@@ -0,0 +1,17 @@
+-- This SQL query selects two columns from a table named "table"
+-- and returns all rows from that table.
+-- The columns selected are "column1" and "column2".
+
+SELECT column1, column2
+FROM table
+
+-- Select all rows from the table
+
+SELECT *
+FROM table
+
+
+-- Select only distinct rows from the table
+SELECT DISTINCT *
+FROM table
+
